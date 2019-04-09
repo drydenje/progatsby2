d94c36ddf463b9d70cceb3b4a77796ad5f36c602
Jeremy Dryden <jeremy.dryden@gmail.com>
2019-04-09 17:24:46 -0400

    Added a progressive web app option

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,10 @@ module.exports = {
     title: `LevelUp Tutorials: Pro Gatsby 2`,
     description: `Working through the LevelUp Tutorials Gatsby 2 guide`,
     author: `@itsmejeremyd`,
+    siteUrl: "https://wonderful-leakey-004d56.netlify.com/",
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -19,26 +21,27 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Level Up Blog`,
+        short_name: `LvlUpBlg`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#524763`,
+        theme_color: `#524763`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     {
-       resolve: 'gatsby-source-filesystem',
-       options: {
-          path: `${__dirname}/src/posts`,
-          name: 'posts',
-       }
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: "posts",
+      },
     },
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`, // this needs to be last in the array
   ],
-}
+};
